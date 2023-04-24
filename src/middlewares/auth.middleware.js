aae0654560b4167985fc8186126b43e4ffd124dd
Letícia Montenegro <montenegro.letiicia@gmail.com>
2023-04-24 17:32:55 -0300

    Updated auth middleware

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -3,9 +3,9 @@ import db from "../database/database.conection.js";
 export default async function authValidation(req, res, next) {
   const { authorization } = req.headers;
   const token = authorization?.replace("Bearer ", "").trim();
-  console.log(authorization);
+
   if (!token) return res.sendStatus(401);
-  console.log(token);
+
   try {
     const session = await db.collection("sessions").findOne({ token });
     if (!session) return res.status(404).send("Nenhum token encontrado");
