4e48421971a28c82028798ce868abddffa01f24a
Letícia Montenegro <montenegro.letiicia@gmail.com>
2023-04-23 22:47:14 -0300

    Organization

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
-import transaction from "../routes/transaction.routes.js";
-import user from "../routes/user.routes.js";
+import transaction from "./transaction.routes.js";
+import user from "./user.routes.js";
 
 const router = Router();
 
