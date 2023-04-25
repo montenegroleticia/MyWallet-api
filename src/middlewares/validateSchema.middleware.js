e1647f88a811c04ac6f2818a10a4345294210249
Letícia Montenegro <montenegro.letiicia@gmail.com>
2023-04-24 23:56:39 -0300

    Updated validateSchema

--- src/middlewares/validateSchema.middleware.js
+++ src/middlewares/validateSchema.middleware.js
@@ -1,8 +1,8 @@
 export default function validateSchema(schema) {
   return (req, res, next) => {
     const validation = schema.validate(req.body, { abortEarly: false });
-    if (validation.erro) {
-      const errors = validation.erro.details.map((detail) => detail.message);
+    if (validation.error) {
+      const errors = validation.error.details.map((detail) => detail.message);
       return res.status(422).send(errors);
     }
     next();
