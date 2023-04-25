64652e33e98b3bcf861c1a53b26c5f5c259b2015
Letícia Montenegro <montenegro.letiicia@gmail.com>
2023-04-25 01:13:28 -0300

    Updated login.schema

--- src/schemas/login.schema.js
+++ src/schemas/login.schema.js
@@ -2,7 +2,7 @@ import joi from "joi";
 
 const loginSchema = joi.object({
   email: joi.string().email().required(),
-  senha: joi.string().required().min(3),
+  senha: joi.string().min(3).required(),
 });
 
 export default loginSchema;
--- src/schemas/user.schema.js
+++ src/schemas/user.schema.js
@@ -3,7 +3,7 @@ import joi from "joi";
 const userSchema = joi.object({
   nome: joi.string().required(),
   email: joi.string().email().required(),
-  senha: joi.string().required().min(3),
+  senha: joi.string().min(3).required(),
 });
 
 export default userSchema;
