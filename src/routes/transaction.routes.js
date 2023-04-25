8b5fd09011d13326a177e1351c565333ece58b3a
Letícia Montenegro <montenegro.letiicia@gmail.com>
2023-04-25 13:33:45 -0300

    Delete Tansaction

--- src/routes/transaction.routes.js
+++ src/routes/transaction.routes.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import {
   postTransacao,
   getHome,
+  deleteTransaction
 } from "../controllers/transaction.controller.js";
 import authValidation from "../middlewares/auth.middleware.js";
 import validateSchema from "../middlewares/validateSchema.middleware.js";
